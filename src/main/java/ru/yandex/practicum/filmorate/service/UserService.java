07bcf1ac702bb6308ee480af403153dc7d5096b4
VadimSamoilov <Vsmirno1988@yandex.ru>
2022-11-03 01:54:55 +0700

    refactor - исправить зависимости в mav

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Slf4j
 public class UserService {
 
-    // private final UserStorage memoryUserStorage;
     private final DBFriendsStorage dbFriendsStorage;
     private final DbUserStorage dbUserStorage;
 
