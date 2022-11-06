a56a6a842cfc7c0a9a234efd6b68ccc11e7764ed
VadimSamoilov <Vsmirno1988@yandex.ru>
2022-11-07 02:19:17 +0700

    Правки

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MPA {
+
+    private long mpaId;
+    private String titleMpa;
+
+}
